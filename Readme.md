ff8ef5bfd3c48b497cb7aed50858b5b9d4da59d0
Arne Maier <Arne_Maier@gmx.de>
2017-01-15 21:58:31 +0100

    Stubs for authentication and callback

---
+++ Readme.md
@@ -0,0 +1,1 @@
+http://www.selfsignedcertificate.com/
--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/authenticate', (req, res) => {
-    res.send('Authenticated');
+app.use(bodyParser.json());
+
+app.post('/authenticate', (req, res) => {
+    debugger;
+    const body = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    const options = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        body: body
+    };
+    fetch(req.body.authUrl, options)
+        .then(data => data.header.cookie)
+        .then(cookie => {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({cookie: cookie}));
+        });
 });
 
-app.listen(PORT, () => {
-console.log(`App available on https://localhost:${PORT}`)
+app.post('/sendToRequestor', (req, res) => {
+    cons body = {
+        uniqueToken: token,
+        cookie: cookie
+    };
+    const options = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    fetch(req.body.cbUrl, options)
+        .then(respond => res.statusCode(request.status).send());
+});
+
+app.use(express.static('app'));
+
+const key = fs.readFileSync('./certs/localhost.key', 'utf8');
+const cert = fs.readFileSync('./certs/localhost.cert', 'utf8');
+const server = https.createServer({key: key, cert: cert}, app);
+
+server.listen(PORT, () => {
+    console.log(`App available on https://localhost:${PORT}`)
 });
